433094df5d74118e01c22639cd209920ef600b56
Yuko Ikeda <a8121020@gsuite.si.aoyama.ac.jp>
2023-05-02 14:34:12 +0900

    ex1-1からprac3までの練習問題を行いました。

--- aandd.js
+++ aandd.js
@@ -1,18 +1,35 @@
- function createSequentialNumbers(array,n){
-                for(var j=1; j<=n; j++){
-                    for(var i=1; i<=j; i++){
+function createSequentialNumbers(array,n){
+                    for(var i=1; i<=n; i++){
                     array.push(i);
                     console.log(array);
                     }
                 }
- }
 
 
-  var random1;
-            function createRandomNumbers(array,n){
+var random1;
+        function createRandomNumbers(array,n){
                 for(var i=0; i<=n; i++){
                     array.push(random1);
                     console.log(array);
                     random1 = Math.floor(Math.random() * 10 + 1);
                 }
             }
+
+function show(obj) {
+            if (Array.isArray(obj)) {
+                var html = '';
+                html = html + '<table border="1">';
+                html = html + '<tr>';
+                for (var i = 0; i < obj.length; i++) {
+                    html = html + '<td>' + obj[i] + '</td>';
+                }
+                html = html + '</tr>';
+                html = html + '</table>';
+                //html = html + '<br>';
+                document.getElementById('res').insertAdjacentHTML('beforeend', html);
+            } else {
+                var string = JSON.stringify(obj);
+                string = string + '<br>';
+                document.getElementById('res').insertAdjacentHTML('beforeend', string);
+            }
+        }
