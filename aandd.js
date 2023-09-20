55b11964deac576b6271dfc1f8e38cf91e4773fe
Yuko Ikeda <a8121020@gsuite.si.aoyama.ac.jp>
2023-09-20 18:11:36 +0900

    prac3-1.htmlまで復習(バイナリーサーチを書く部分以外)

--- aandd.js
+++ aandd.js
@@ -1,34 +1,32 @@
-function createSequentialNumbers(array,n){
-                    for(var i=1; i<=n; i++){
-                    array.push(i)*i;
-                    console.log(array);
-                    }
-                }
+function createSequentialNumbers(array, n) {
+    for (var i = 1; i <= n; i++) {
+        for (var j = 0; j < i; j++) {
+            array.push(i);
+        }
+    }
+}
 
-
-        function createRandomNumbers(array,n){
-                for(var i=0; i<n; i++){
-                    var random1 = Math.floor(Math.random()*10 + 1)
-                    array.push(random1);
-                    console.log(array);
-                }
-            }
+function createRandomNumbers(array, n) {
+    for (var i = 0; i < n; i++) {
+        array.push(Math.round(Math.random() * 100));
+    }
+}
 
 function show(obj) {
-            if (Array.isArray(obj)) {
-                var html = '';
-                html = html + '<table border="1">';
-                html = html + '<tr>';
-                for (var i = 0; i < obj.length; i++) {
-                    html = html + '<td>' + obj[i] + '</td>';
-                }
-                html = html + '</tr>';
-                html = html + '</table>';
-                //html = html + '<br>';
-                document.getElementById('res').insertAdjacentHTML('beforeend', html);
-            } else {
-                var string = JSON.stringify(obj);
-                string = string + '<br>';
-                document.getElementById('res').insertAdjacentHTML('beforeend', string);
-            }
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
         }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
+    }
+}
