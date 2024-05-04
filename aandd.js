922fe1b9b9d5ce773cae51a2a2e08d9e4d7fb7ed
Yuko Ikeda <a8121020@gsuite.si.aoyama.ac.jp>
2024-05-04 23:37:38 +0900

    バイナリーサーチのプログラムを追加

--- aandd.js
+++ aandd.js
@@ -1,32 +1,46 @@
 function createSequentialNumbers(array, n) {
-    for (var i = 1; i <= n; i++) {
-        for (var j = 0; j < i; j++) {
-            array.push(i);
-        }
+  for (var i = 1; i <= n; i++) {
+    for (var j = 0; j < i; j++) {
+      array.push(i);
     }
+  }
 }
 
 function createRandomNumbers(array, n) {
-    for (var i = 0; i < n; i++) {
-        array.push(Math.round(Math.random() * 100));
+  for (var i = 0; i < n; i++) {
+    array.push(Math.round(Math.random() * 100));
+  }
+}
+
+//ランダムな数を昇順に並び替える
+function createOrderedRandomNumbers(numbers, n) {
+  var count = 0;
+  for (var i = 1; ; i++) {
+    if (Math.floor(Math.random() * 3) == 0) {
+      numbers.push(i);
+      count++;
+      if (count >= n) {
+        break;
+      }
     }
+  }
 }
 
 function show(obj) {
-    if (Array.isArray(obj)) {
-        var html = '';
-        html = html + '<table border="1">';
-        html = html + '<tr>';
-        for (var i = 0; i < obj.length; i++) {
-            html = html + '<td>' + obj[i] + '</td>';
-        }
-        html = html + '</tr>';
-        html = html + '</table>';
-        //html = html + '<br>';
-        document.getElementById('res').insertAdjacentHTML('beforeend', html);
-    } else {
-        var string = JSON.stringify(obj);
-        string = string + '<br>';
-        document.getElementById('res').insertAdjacentHTML('beforeend', string);
+  if (Array.isArray(obj)) {
+    var html = "";
+    html = html + '<table border="1">';
+    html = html + "<tr>";
+    for (var i = 0; i < obj.length; i++) {
+      html = html + "<td>" + obj[i] + "</td>";
     }
+    html = html + "</tr>";
+    html = html + "</table>";
+    //html = html + '<br>';
+    document.getElementById("res").insertAdjacentHTML("beforeend", html);
+  } else {
+    var string = JSON.stringify(obj);
+    string = string + "<br>";
+    document.getElementById("res").insertAdjacentHTML("beforeend", string);
+  }
 }
